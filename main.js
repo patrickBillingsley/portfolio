de4bd176460a5ae13e7d8147fb7e7864a4a6897e
sadSack <prbillingsley89@gmail.com>
2020-09-20 19:34:09 -0500

    Add transitions to about section
    *Add new IntersectionObserver to about section
    *Add appear class in CSS
    *Add transform, opacity, and transition information to about
    	section

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ window.addEventListener('scroll', () => {
 }, false);
 
 const navItems = document.querySelectorAll('.anim');
+const aboutItems = document.querySelectorAll('.about__card');
 
 const optionsDown = {
     root: document.querySelector('body'),
@@ -13,7 +14,10 @@ const optionsUp = {
     root: document.querySelector('body'),
     rootMargin: '0px 0px -80% -15%',
     threshold: 0.5
-}
+};
+const appearOptions =  {
+    threshold: 0.25
+};
 
 const scrollFollowDown = new IntersectionObserver(function(entries) {
     entries.forEach(entry => {
@@ -31,8 +35,21 @@ const scrollFollowUp = new IntersectionObserver(function(entries) {
         entry.target.classList.toggle(`nav__${entry.target.innerText}--hor`);
     });
 }, optionsUp);
+const appearOnScroll = new IntersectionObserver(function(entries, appearOnScroll) {
+    entries.forEach(entry => {
+        if(!entry.isIntersecting) {
+            return;
+        }
+        entry.target.classList.add('appear');
+        appearOnScroll.unobserve(entry.target);
+    })
+}, appearOptions);
+
 
 navItems.forEach(navItem => {
     scrollFollowDown.observe(navItem);
     scrollFollowUp.observe(navItem);
-})
+});
+aboutItems.forEach(aboutItem => {
+    appearOnScroll.observe(aboutItem);
+});
