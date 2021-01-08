b67ff3e97ecdc00e7432001bf4d4454f002a7c4e
Patrick Billingsley <Patrick@Kristens-iMac.local>
2021-01-08 08:47:22 -0600

    Refactor Nav Animations

--- main.js
+++ main.js
@@ -2,39 +2,38 @@ window.addEventListener('scroll', () => {
     document.body.style.setProperty('--scroll', window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
 }, false);
 
+console.log(document.body.style);
+
 const navItems = document.querySelectorAll('.anim');
 const aboutItems = document.querySelectorAll('.about__card');
 
-const optionsDown = {
+const scrollOptions = {
     root: document.querySelector('body'),
-    rootMargin: '-20% -75% 0px 0px',
-    threshold: 0.5
-};
-const optionsUp = {
-    root: document.querySelector('body'),
-    rootMargin: '0px 0px -80% -15%',
-    threshold: 0.5
+    rootMargin: '-25% 0px 0px 0px',
+    threshold: 1
 };
 const appearOptions =  {
     threshold: 0.25
 };
 
-const scrollFollowDown = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-        if(!entry.isIntersecting) {
-            return;
-        }
-        entry.target.classList.toggle(`nav__${entry.target.innerText}--hor`);
-    });
-}, optionsDown);
-const scrollFollowUp = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-        if(!entry.isIntersecting) {
-            return;
-        }
-        entry.target.classList.toggle(`nav__${entry.target.innerText}--hor`);
-    });
-}, optionsUp);
+// const scrollFollow = new IntersectionObserver(entries => {
+//     entries.forEach(entry => {
+//         const className = `nav-bar__${entry.target.innerText}--hor`;
+//         const classList = entry.target.classList;
+//         if(!entry.isIntersecting) {
+//             if(classList.contains(className)){
+//                 classList.remove(className);
+//                 navReturn(entry.target);
+//             }
+//             return;
+//         } 
+//         if(!classList.contains(className)){
+//             classList.add(className);
+//             navSplit(entry.target);
+//         }
+//     });
+// }, scrollOptions);
+
 const appearOnScroll = new IntersectionObserver((entries, appearOnScroll) => {
     entries.forEach(entry => {
         if(!entry.isIntersecting) {
@@ -46,10 +45,18 @@ const appearOnScroll = new IntersectionObserver((entries, appearOnScroll) => {
 }, appearOptions);
 
 
-navItems.forEach(item => {
-    scrollFollowDown.observe(item);
-    scrollFollowUp.observe(item);
-});
+// navItems.forEach(item => {
+//     scrollFollow.observe(item);
+// });
 aboutItems.forEach(item => {
     appearOnScroll.observe(item);
 });
+
+// function navSplit(target) {
+//     let width = target.offsetWidth * 1.15;
+//     let position = `${target.dataset.position * width}px`;
+    // target.setAttribute('style', `transform: translateX(${position})`);
+// };
+// function navReturn(target) {
+    // target.setAttribute('style', 'transform: initial');
+// }
