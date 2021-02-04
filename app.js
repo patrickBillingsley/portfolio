cfd8d1a441d9ac2e5c2bafce06e0ab0da85be908
Patrick Billingsley <Patrick@Kristens-iMac.local>
2021-02-04 11:49:16 -0600

    Implement Home Tab
    *Add Home section title
    *Reassign tab numbers
    *Refactor section title CSS to alot for Home section title
    *Refactor Javascript to alot for Home section title

--- app.js
+++ app.js
@@ -2,17 +2,31 @@ const tabs = Array.from(document.querySelectorAll('[data-tab]'));
 
 tabs.forEach(tab => {
     tab.addEventListener('click', event => {
-        tabNum = event.target.dataset.tab;
+        const tabNum = event.target.dataset.tab;
 
         tabs.forEach(tab => {
+            const classes = tab.offsetParent.classList;
+
+            if(tabNum == 1) {
+                if(tabs[0].classList.contains('--hidden')) {
+                    tabs[0].classList.remove('--hidden');
+                }
+            } else {
+                if(!tabs[0].classList.contains('--hidden')) {
+                    tabs[0].classList.add('--hidden');
+                }
+            }
             if(tabs.indexOf(tab) < tabNum) {
-                if(tab.offsetParent.classList.contains('--hidden')) {
-                    tab.offsetParent.classList.remove('--hidden');
+                if(classes.contains('--hidden')) {
+                    classes.remove('--hidden');
                 }
             }
             if(tabs.indexOf(tab) >= tabNum) {
-                if(!tab.offsetParent.classList.contains('--hidden')) {
-                    tab.offsetParent.classList.add('--hidden');
+                // if(!tabs[0].classList.contains('--hidden')) {
+                //     tabs[0].classList.add('--hidden');
+                // }
+                if(!classes.contains('--hidden')) {
+                    classes.add('--hidden');
                 }
             }
         })
