c4d74102d1f0b0885d56bc617bee18203b3e576e
Patrick Billingsley <Patrick@Kristens-iMac.local>
2021-02-05 12:28:03 -0600

    Begin Styling Projects and Implementing Interactivity
    *Add event listeners to projects tabs
    *Remove unnecessary HTML from Projects section

--- app.js
+++ app.js
@@ -22,11 +22,40 @@ tabs.forEach(tab => {
                 }
             }
             if(tabs.indexOf(tab) >= tabNum) {
-                // if(!tabs[0].classList.contains('--hidden')) {
-                //     tabs[0].classList.add('--hidden');
-                // }
                 if(!classes.contains('--hidden')) {
                     classes.add('--hidden');
+                }
+            }
+        })
+    })
+})
+
+const projects = Array.from(document.querySelectorAll('[data-project]'))
+
+projects.forEach(project => {
+    project.addEventListener('click', event => {
+        const tagName = event.target.tagName;
+        
+        let projectNum;
+
+        if(tagName == 'DIV') {
+            projectNum = event.target.dataset.project;
+        } else {
+            projectNum = event.target.parentElement.dataset.project;
+        }
+
+        projects.forEach(project => {
+            const classes = project.classList;
+            const className = 'projects__section--hidden';
+
+            if(projects.indexOf(project) == projectNum - 1) {
+                if(classes.contains(className)) {
+                    classes.remove(className);
+                }
+            }
+            if(projects.indexOf(project) !== projectNum - 1) {
+                if(!classes.contains(className)) {
+                    classes.add(className);
                 }
             }
         })
